d87ce61c6e2edb28b60bcfd27b52f8d1110fbdb6
Pavlo Neshataiev <pavelcreator@gmail.com>
2026-02-04 13:36:15 -0700

    images add

---
+++ src/app/components/game-wrapper/game-wrapper.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-game-wrapper',
+    standalone: true,
+    templateUrl: 'game-wrapper.component.html',
+    styleUrls: ['./game-wrapper.component.css'],
+})
+export class GameWrapperComponent {}
